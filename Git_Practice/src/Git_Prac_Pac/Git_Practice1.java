4e8bc5500dfaec26ae11dfbcf9a7981f937ff127
Lenovo <Lenovo@LAPTOP-SIG5U20R>
2024-08-11 10:20:43 +0530

    Pushing the code first time.

---
+++ Git_Practice/src/Git_Prac_Pac/Git_Practice1.java
@@ -0,0 +1,13 @@
+package Git_Prac_Pac;
+
+public class Git_Practice1 {
+	int Deepak = 12;
+
+	public static void main(String[] args) {
+		System.out.println("This is a practice class");
+		Git_Practice1 ob = new Git_Practice1();
+		int c=ob.Deepak;
+		System.out.println(c);
+			}
+
+}
