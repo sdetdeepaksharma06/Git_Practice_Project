974abacf861bd4aa41a0112647d153c1cb78cb52
Lenovo <Lenovo@LAPTOP-SIG5U20R>
2024-08-11 10:27:14 +0530

    Second Commit

---
+++ Git_Practice/src/Git_Prac_Pac/Git_Practice2.java
@@ -0,0 +1,11 @@
+package Git_Prac_Pac;
+
+public class Git_Practice2 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git");
+		System.out.println("This is second class");
+
+	}
+
+}
